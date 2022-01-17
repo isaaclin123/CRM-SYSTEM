74ea65774264e3c4194afdd12564565c9dbcaf51
isaac lin <blin510@aucklanduni.ac.nz>
2022-01-17 15:28:30 +1300

    commit

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const app = express();
 const port = process.env.PORT||3000;
 const sanitizeHtml = require('sanitize-html');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
 
 
 
